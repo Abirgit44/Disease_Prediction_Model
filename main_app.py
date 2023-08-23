1ab307227706234f86607ae6691725dfdcd6ec4c
Abir Maiti <abirmaiti56@gmail.com>
2023-08-23 17:02:11 +0530

    path update
    updated path of models which changed in local file

--- main_app.py
+++ main_app.py
@@ -9,9 +9,9 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-diab_model = pickle.load(open('C:/Users/91771/Healthcare_Prediction_Project/saved_models/diab_model.sav', 'rb'))
-heart_model = pickle.load(open("C:/Users/91771/Healthcare_Prediction_Project/saved_models/heart_model.sav",'rb'))
-parkinsons_model = pickle.load(open("C:/Users/91771/Healthcare_Prediction_Project/saved_models/parkinsons_model.sav", 'rb'))
+diab_model = pickle.load(open('C:/Users/91771/Disease_Prediction_Model/saved_models/diab_model.sav', 'rb'))
+heart_model = pickle.load(open("C:/Users/91771/Disease_Prediction_Model/saved_models/heart_model.sav",'rb'))
+parkinsons_model = pickle.load(open("C:/Users/91771/Disease_Prediction_Model/saved_models/parkinsons_model.sav", 'rb'))
 
 st.title("Disease Prediction App")
 
